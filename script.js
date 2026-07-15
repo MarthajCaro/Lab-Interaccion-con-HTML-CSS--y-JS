b653194d9bd3f7970af77cd64ce0c487bdefd6dd
Martha <caromartha175@gmail.com>
2026-07-15 14:02:03 -0500

    se hicieron ajustes en el código para mejorar la apariencia

--- script.js
+++ script.js
@@ -10,15 +10,44 @@ function restar() {
     actualizar()
 }
 
+function actualizar() {
+    const elemento = document.getElementById("valor");
+    elemento.textContent = valor;
+
+  // 🔥 condición del ejercicio
+    if (valor >= 10) {
+    elemento.style.color = "lime"; // verde
+    } else {
+    elemento.style.color = "white"; // normal
+    }
+
 function evaluar() {
     const nombre = document.getElementById("nombre").value;
     const resultado = document.getElementById("resultado");
 
     if (valor >= 10) {
-    resultado.textContent = `${nombre}, tienes energía máxima ⚡🔥`;
+    resultado.textContent = `${nombre}, estás en tu máximo nivel ⚡🔥`;
     } else if (valor >= 5) {
-    resultado.textContent = `${nombre}, vas bien 💪`;
+    resultado.textContent = `${nombre}, buen nivel de energía 💪`;
     } else {
-    resultado.textContent = `${nombre}, necesitas más energía 😴`;
+    resultado.textContent = `${nombre}, sube tu nivel de energía ⚡😴`;
     }
+    
+
+
+function verEstado() {
+    let mensaje = "";
+
+    if (valor <= 0) {
+        mensaje = "😴 Estás sin energía";
+    } else if (valor < 10) {
+        mensaje = "🙂 Vas subiendo tu energía";
+    } else {
+        mensaje = "⚡ ¡Energía al máximo!";
+    }
+
+    document.getElementById("mensaje").textContent = mensaje;
+}
+}
+
 }
